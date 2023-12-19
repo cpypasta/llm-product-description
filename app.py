2da9871e466448376dc1b1ad05ffce5f40b69734
Brian Roller <brianroller@brians-mbp.lan>
2023-12-19 15:48:07 -0600

    streamlit

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ Sources of data:
 1. Internet
 2. Product Manuals (PDFs)
 """
-from timeit import default_timer as default_timer
+import streamlit as st
 from dotenv import load_dotenv
 from web_research import WebResearchRetriever
 from product_description import create_product_description_chain
 from langchain_core.language_models.llms import LLM
 from langchain.chat_models import ChatOpenAI
 from langchain.llms.openai import OpenAI
-from langchain.schema import StrOutputParser, Document
+from langchain.schema import Document
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 
@@ -38,12 +38,29 @@ def create_product_description(llm: LLM, product: str, docs: list[Document]) -> 
   description = chain.invoke({"product": product, "context": joined_docs})
   return f"{description}\n\n{_format_sources(docs)}"
 
-product = "BUC10986"
-query = f"what is the product description for product ID {product}"
-chroma = Chroma(embedding_function=OpenAIEmbeddings(), collection_name="search_results")
-retriever = WebResearchRetriever.from_llm(llm_fast, chroma)
-search_docs = retriever.get_relevant_documents(query)
-description = create_product_description(llm, product, search_docs)
-print(description)
+def run() -> None:
+  product = st.session_state.product_id
+  query = f"what is the product description for product ID {product}"
+  chroma = Chroma(embedding_function=OpenAIEmbeddings(), collection_name="search_results")
+  with st.status("Generating description...", expanded=True) as status:
+    st.write("Searching internet")
+    retriever = WebResearchRetriever.from_llm(llm_fast, chroma)
+    search_docs = retriever.get_relevant_documents(query)
+    st.write("Processing description") 
+    description = create_product_description(llm, product, search_docs)  
+    status.update(label="Description Created", state="complete", expanded=False)
+  chroma.delete_collection()
+  st.session_state.description = description
 
-chroma.delete_collection()
+def generate_clicked():
+  run()
+
+if __name__ == "__main__":    
+  st.title("Product Description Generator")
+  st.info("Enter a product ID to generate a product description.")
+  st.text_input("Product ID", value="BUC10986", key="product_id")
+  st.button("Generate", on_click=generate_clicked)
+  
+  if "description" in st.session_state:
+    st.write(st.session_state.description)
+    del st.session_state.description
