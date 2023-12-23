731fa0d518daeee3d95b5ac9143383769aeceaf1
Brian Roller <brianroller@brians-mbp.lan>
2023-12-22 20:06:18 -0600

    adding more search options

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ Sources of data:
 1. Internet
 2. Product Manuals (PDFs)
 """
-import streamlit as st
+import streamlit as st, re
+from time import time
 from dotenv import load_dotenv
-from web_research import WebResearchRetriever
+from streamlit.elements.lib.mutable_status_container import StatusContainer
+from streamlit.delta_generator import DeltaGenerator
+from web_research import WebResearchRetriever, WebScrapeMethod
 from product_description import create_product_description_chain
 from langchain_core.language_models.llms import LLM
 from langchain.chat_models import ChatOpenAI
@@ -17,50 +20,140 @@ from langchain.llms.openai import OpenAI
 from langchain.schema import Document
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
+from langchain.callbacks.base import BaseCallbackHandler
 
 load_dotenv()
-llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
-llm_fast = OpenAI(model="text-davinci-003", temperature=0)
 
-
-def _format_sources(docs: list[Document]) -> str:
-  """Format sources as a string, with each source on a new line."""
+class StreamHandler(BaseCallbackHandler):
+  def __init__(self, container: DeltaGenerator, initial_text: str = ""):
+    self.container = container
+    self.text=initial_text
+  def on_llm_new_token(self, token: str, **kwargs) -> None:
+    self.text += token
+    self.container.markdown(self.text)
+        
+def _get_sources(docs: list[Document]) -> list[str]:
+  """Get sources from documents."""
   sources = set()
   for doc in docs:
-    sources.add(doc.metadata.get("source", "unknown"))
-      
+    new_sources = doc.metadata.get("source", "unknown")
+    new_sources = new_sources.split(";")
+    for s in new_sources:
+      sources.add(s)
+  return list(sources)
+        
+def _format_sources(sources: list[str]) -> str:
+  """Format sources as a string, with each source on a new line."""
   sources_str = "\n".join([f"* {s}" for s in sources])
   return f"Sources:\n{sources_str}"
 
-def create_product_description(llm: LLM, product: str, docs: list[Document]) -> str:
+def create_product_description(llm: LLM, product: str, docs: list[Document]) -> list[str]:
   chain = create_product_description_chain(llm)   
   joined_docs = "\n\n".join([doc.page_content for doc in docs])
-  description = chain.invoke({"product": product, "context": joined_docs})
-  return f"{description}\n\n{_format_sources(docs)}"
+  chain.invoke({"product": product, "context": joined_docs})
+  return _get_sources(docs)
 
-def run() -> None:
-  product = st.session_state.product_id
-  query = f"what is the product description for product ID {product}"
+def run(
+  llm: LLM, 
+  llm_fast: LLM, 
+  description_status: StatusContainer, 
+  product: str,
+  web_scrape_method: WebScrapeMethod,
+  smart_top_links: bool,
+  links_n: int,
+  vector_n: int
+) -> str:
+  start_time = time()
+  query = f"what is the product {product}"
   chroma = Chroma(embedding_function=OpenAIEmbeddings(), collection_name="search_results")
-  with st.status("Generating description...", expanded=True) as status:
+  with description_status as status:
     st.write("Searching internet")
-    retriever = WebResearchRetriever.from_llm(llm_fast, chroma)
+    retriever = WebResearchRetriever.from_llm(
+      llm_fast, 
+      chroma, 
+      web_scrape_method, 
+      smart_top_links,
+      links_n,
+      vector_n
+    )
     search_docs = retriever.get_relevant_documents(query)
     st.write("Processing description") 
-    description = create_product_description(llm, product, search_docs)  
-    status.update(label="Description Created", state="complete", expanded=False)
+    sources = create_product_description(llm, product, search_docs) 
+    end_time = time()
+    end_time = time()
+    duration = end_time - start_time 
+    duration = f"{duration:.2f} secs"
+    status.update(label=f"Description Created ({duration})", state="complete", expanded=False)
   chroma.delete_collection()
-  st.session_state.description = description
+  return sources
 
-def generate_clicked():
-  run()
 
 if __name__ == "__main__":    
   st.title("Product Description Generator")
   st.info("Enter a product ID to generate a product description.")
-  st.text_input("Product ID", value="BUC10986", key="product_id")
-  st.button("Generate", on_click=generate_clicked)
+  product_id = st.text_input("Product ID", value="BUC10986", key="product_id")
+  generate_btn = st.button("Generate")
   
-  if "description" in st.session_state:
-    st.write(st.session_state.description)
-    del st.session_state.description
+  # OPTIONS
+  with st.sidebar:
+    chat_model = st.selectbox(
+      "Chat Model",
+      options=[
+        "gpt-4-1106-preview", 
+        "gpt-4", 
+        "gpt-3.5-turbo-16k-0613", 
+        "gpt-3.5-turbo-16k", 
+        "gpt-3.5-turbo-1106",
+        "gpt-3.5-turbo"
+      ],
+      index=1
+    )
+    chat_temp = st.slider("Chat temperature (creativity)", 0.0, 2.0, 0.7, 0.1)
+    web_scrape_method = st.selectbox(
+      "Web Scrape Method", 
+      options=["simple", "http", "browser", "screenshot"], 
+      index=1
+    )
+    smart_top_links = st.toggle("Smart top links", value=True)
+    links_n = st.slider("Number of links to search", 1, 10, 3)
+    vector_n = st.slider("Number of vector results", 1, 15, 5) 
+  
+  # GENERATE DESCRIPTION
+  if product_id and generate_btn:
+    description_status = st.status("Generating description...", expanded=True)
+    chat_box = st.empty()
+    stream_handler = StreamHandler(chat_box)
+    print("using", chat_model)
+    llm = ChatOpenAI(
+      model=chat_model, 
+      temperature=chat_temp, 
+      streaming=True, 
+      callbacks=[stream_handler]
+    )
+    llm_fast = OpenAI(model="text-davinci-003", temperature=0)
+    sources = run(
+      llm, 
+      llm_fast, 
+      description_status, 
+      product_id, 
+      WebScrapeMethod(web_scrape_method),
+      smart_top_links,
+      links_n,
+      vector_n
+    )
+    st.write(_format_sources(sources))
+    if web_scrape_method == "screenshot":
+      with st.expander("Screenshots"):
+        domain = r"https?://([A-Za-z_0-9.-]+).*"
+        source_domains = []
+        for i, s in enumerate(sources):
+          if re.match(domain, s):
+            d = re.match(domain, s).group(1)
+            d = d.replace("www.", "")
+            source_domains.append(d)
+          else:
+            source_domains.append(f"Screenshot {i}")
+            
+        tabs = st.tabs(source_domains)
+        for i, tab in enumerate(tabs):
+          tab.image(f"screenshots/image{i}.png")
--- web_research.py
+++ web_research.py
@@ -1,7 +1,9 @@
-import json
+import json, base64, openai
+from enum import Enum
 from timeit import default_timer
 from typing import Callable, Any
 from json_parser import JsonOutputParser
+from playwright.sync_api import sync_playwright
 from langchain_core.documents import Document
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.callbacks import CallbackManagerForRetrieverRun
@@ -24,14 +26,40 @@ def timer(func: Callable) -> Any:
     return result
   return inner
 
+class WebScrapeMethod(str, Enum):
+  simple = "simple"
+  http = "http"
+  browser = "browser"
+  screenshot = "screenshot"
+
 class WebResearchRetriever(BaseRetriever):  
   """Will use Google search to find the top 3 relevant web pages and return the most relevant document results."""
+  
   llm: BaseLLM = Field(..., description="LLM used to ask NLP questions")
   vector_store: VectorStore = Field(..., description="Vector store used to find relevant documents")
+  web_scrape_method: WebScrapeMethod = Field(WebScrapeMethod.http, description="Method used to scrape web pages")
+  smart_top_links: bool = Field(True, description="Whether to use the LLM to find the top links")
+  links_n: int = Field(3, description="Number of links to return")
+  vector_n: int = Field(5, description="Number of vectors to return")
   
   @classmethod
-  def from_llm(self, llm: BaseLLM, vector_store: VectorStore) -> "WebResearchRetriever":
-    return self(llm=llm, vector_store=vector_store)
+  def from_llm(
+    self, 
+    llm: BaseLLM, 
+    vector_store: VectorStore,
+    web_scrape_method: WebScrapeMethod,
+    smart_top_links: bool,
+    links_n: int,
+    vector_n: int
+  ) -> "WebResearchRetriever":
+    return self(
+      llm=llm, 
+      vector_store=vector_store, 
+      web_scrape_method=web_scrape_method,
+      smart_top_links=smart_top_links,
+      links_n=links_n,
+      vector_n=vector_n
+    )
   
   def _parse_search_results(self, results: list[dict]) -> list[dict]:
       """Parse search results and return title, snippet, and link."""
@@ -47,16 +75,23 @@ class WebResearchRetriever(BaseRetriever):
 
   @timer
   def _web_search(self, query: str) -> list[dict]:
-      """Search for query on Google and return results."""
-      search_result = GoogleSerperAPIWrapper().results(query)
-      return self._parse_search_results(search_result.get("organic"))
+    """Search for query on Google and return results."""
+    search_result = GoogleSerperAPIWrapper().results(query)
+    if self.web_scrape_method == WebScrapeMethod.simple:
+      if "answerBox" in search_result:
+        answer = search_result["answerBox"]
+      else:
+        answer = search_result["organic"][0]
+      return self._parse_search_results([answer])
+    else:
+      return self._parse_search_results(search_result["organic"])
 
   @timer
   def _fine_best_web_sources(self, llm: BaseLLM, query: str, search_result: list[dict]) -> list[str]:
     """Find the best web sources from the search results."""
     search_dump = json.dumps(search_result, indent=2)
     best_search_prompt = PromptTemplate.from_template(
-      """You are an expert at looking at search results and picking the best ones. You will be given the <query> and the top 10 <results>. Each result will include <title> and <description> and <link>. Using the search result details, you will pick the top results based on how likely the result will have information related to the <query>. Remember, only return the top 3 results, and respond with a JSON list of <link> values.
+      """You are an expert at looking at search results and picking the best ones. You will be given the <query> and the top 10 <results>. Each result will include <title> and <description> and <link>. Using the search result details, you will pick the top results based on how likely the result will have information related to the <query>. Remember, only return the top {links_n} results, and respond with a JSON list of <link> values.
       QUERY:
       {query}
 
@@ -65,11 +100,20 @@ class WebResearchRetriever(BaseRetriever):
       {results}
       '''
 
-      TOP 3 RESULTS:                                                  
+      TOP {links_n} RESULTS:                                                  
       """)
 
     chain = best_search_prompt | llm | JsonOutputParser()
-    best_links = chain.invoke({"query": query, "results": search_dump}) 
+    best_links = chain.invoke({"query": query, "results": search_dump, "links_n": self.links_n}) 
+    return best_links
+
+  def _find_best_links(self, query: str, search_results: list[dict]):
+    if self.links_n > len(search_results):
+      best_links = [x["link"] for x in search_results]
+    elif self.smart_top_links:
+      best_links = self._fine_best_web_sources(self.llm, query, search_results)
+    else:
+      best_links = [x["link"] for x in search_results[:self.links_n]]
     return best_links
 
   @timer
@@ -80,7 +124,7 @@ class WebResearchRetriever(BaseRetriever):
     return search_docs
 
   @timer
-  def _web_scrape(self, best_links: list[str]) -> list[Document]:
+  def _browser_scrape(self, best_links: list[str]) -> list[Document]:
     """Web scraping using a headless browser."""
     web_loader = AsyncChromiumLoader(best_links)
     search_docs = web_loader.load()
@@ -91,23 +135,91 @@ class WebResearchRetriever(BaseRetriever):
     )
     return docs_transformed
 
+  def _take_screenshots(self, urls: str) -> list[str]:
+    filenames = []
+    with sync_playwright() as p:
+      browser = p.chromium.launch()
+      for i, url in enumerate(urls):
+        page = browser.new_page()
+        page.goto(url)
+        filename = f"screenshots/image{i}.png"
+        page.screenshot(path=filename, full_page=True)
+        filenames.append(filename)
+      browser.close()
+    return filenames
+      
+  def _extract_description_from_images(self, filenames: list[str]) -> str:
+    image_messages = []
+    for f in filenames:
+      with open(f, "rb") as f:
+        b64_image = base64.b64encode(f.read()).decode() 
+      image_messages.append({
+        "type": "image",
+        "image_url": {
+          "url": f"data:image/png;base64,{b64_image}"
+        }
+      })
+    content = [{
+      "type": "text",
+      "text": f"Find information about the main product:"
+    }]
+    content.extend(image_messages)
+    
+    messages = [
+      {
+        "role": "system",
+        "content": """You are a web scraper, whose job is to extract information about a product from website screenshots. The intention is to extract the product details that can be useful to describe the product. DO NOT include details about return policies, warranty information, or price."""
+      },
+      {
+        "role": "user",
+        "content": content
+      }
+    ]
+    
+    llm = openai.Client()
+    response = llm.chat.completions.create(
+      model="gpt-4-vision-preview",
+      messages = messages,
+      max_tokens=4000
+    )
+    message = response.choices[0].message
+    return message.content
+    
+  @timer
+  def _screenshot_scrape(self, best_links: list[str]) -> list[Document]:
+    filenames = self._take_screenshots(best_links)
+    description = self._extract_description_from_images(filenames)
+    return [Document(page_content=description, metadata={"source": ";".join(best_links)})]
+
   @timer
   def _find_relevant_search_docs(self, search_docs: list[Document], query: str) -> list[Document]:
     """Find the most relevant text within the search docs."""
     split_docs = RecursiveCharacterTextSplitter().split_documents(search_docs)  
     self.vector_store.add_documents(split_docs)
-    related_docs = self.vector_store.similarity_search(f"what is the product description for {query}", k=10)
+    related_docs = self.vector_store.similarity_search(f"what is the product description for {query}", k=self.vector_n)
     return related_docs
+  
+  def _web_scrape(self, best_links: list[str], search_results: list[Document]):
+    if self.web_scrape_method == WebScrapeMethod.http:
+      return self._fast_web_scrape(best_links)
+    elif self.web_scrape_method == WebScrapeMethod.browser:
+      return self._browser_scrape(best_links)
+    elif self.web_scrape_method == WebScrapeMethod.screenshot:
+      return self._screenshot_scrape(best_links)
+    elif self.web_scrape_method == WebScrapeMethod.simple:
+      return [Document(page_content=search_results[0]["snippet"], metadata={"source": best_links[0]})]
+    else:
+      raise ValueError(f"Unknown web scrape method: {self.web_scrape_method}")
   
   def get_relevant_documents(
     self, 
-    query: str, 
+    query: str,     
     *, 
     run_manager: CallbackManagerForRetrieverRun
   ) -> list[Document]:
     """Search Google for documents related to the query input."""
     search_results = self._web_search(query)
-    best_links = self._fine_best_web_sources(self.llm, query, search_results)
-    search_docs = self._fast_web_scrape(best_links)
+    best_links = self._find_best_links(query, search_results)
+    search_docs = self._web_scrape(best_links, search_results)
     search_docs = self._find_relevant_search_docs(search_docs, query)    
     return search_docs
